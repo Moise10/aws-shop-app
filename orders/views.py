f681b1488b0c580e5c3f375191271311a0fbba66
moise <mbakopmoise237@gmail.com>
2023-10-27 18:24:20 +0800

    exporting orders to a csv file functionality

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
-from .models import OrderItem
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.admin.views.decorators import staff_member_required
+from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from .tasks import order_created
@@ -30,6 +31,7 @@ def order_create(request):
     form = OrderCreateForm()
   return render(request,'orders/order/create.html',
                       {'cart': cart, 'form': form})
+
 
 
 

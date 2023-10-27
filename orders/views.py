0d5a416debcedbb51a8cdc6412c422a26944f2cb
moise <mbakopmoise237@gmail.com>
2023-10-28 03:21:23 +0800

    modified the administration site with a custome view

--- orders/views.py
+++ orders/views.py
@@ -33,6 +33,11 @@ def order_create(request):
                       {'cart': cart, 'form': form})
 
 
-
+@staff_member_required
+def admin_order_detail(request, order_id):
+  order = get_object_or_404(Order, id=order_id)
+  return render(request,
+              'admin/orders/order/detail.html',
+              {'order': order})
 
 

960116fdf5cd8e26c59569921b9960a1bdbb2dbf
moise <mbakopmoise237@gmail.com>
2023-10-28 15:04:47 +0800

    adding a coupon system

--- orders/views.py
+++ orders/views.py
@@ -42,7 +42,11 @@ def order_create(request):
   if request.method == "POST":
     form = OrderCreateForm(request.POST)
     if form.is_valid():
-      order = form.save()
+      order = form.save(commit = False)
+      if cart.coupon:
+        order.coupon = cart.coupon
+        order.discount = cart.coupon.discount
+      order.save()
       for item in cart:
         OrderItem.objects.create(order=order,
                             product=item['product'],
@@ -50,16 +54,20 @@ def order_create(request):
                             quantity=item['quantity'])
       #clear the cart
       cart.clear()
+
       # launch asynchronous task
       order_created.delay(order.id)
+
       # set the order in the session
       request.session['order_id'] = order.id
+      
       # redirect for payment
     return redirect(reverse('payment:process'))
   else:
     form = OrderCreateForm()
   return render(request,'orders/order/create.html',
                       {'cart': cart, 'form': form})
+
 
 
 @staff_member_required
